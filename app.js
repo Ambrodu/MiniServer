d7c889513bd42dd48433a2d35faf027d8ce4c6db
Ambrodu <Ambrodu@gmail.com>
2022-04-22 17:28:10 +0200

    Edit

--- app.js
+++ app.js
@@ -11,7 +11,12 @@ app.use(noBots({block:true}));
 app.get('*', (req, res) => {
 	if (fs.existsSync(path.join(__dirname + '/public/' + req.headers.host))) {
 		if(req.url == '/'){
-			res.sendFile(path.join(__dirname + '/public/' + req.headers.host + req.url + '/index.html'));
+			if (fs.existsSync(path.join(__dirname + '/public/' + req.headers.host + req.url + '/index.html'))) {
+				res.sendFile(path.join(__dirname + '/public/' + req.headers.host + req.url + '/index.html'));
+			}
+			else{
+				res.sendFile(path.join(__dirname + '/public/404.html'));
+			}
 		}
 		else{
 			if (fs.existsSync(path.join(__dirname + '/public/' + req.headers.host + req.url))) {
